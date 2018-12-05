3034f73a77d1f8df526ee6655b129bcb1ce34d41
Tyler Coles <tcoles@hpfc.com>
2018-12-05 13:39:11 -0500

    Finished Day 3

---
+++ AdventCalendar/Day3/FabricClaim.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AdventCalendar.Day3
+{
+    public class FabricClaim
+    {
+        Regex claimRegex = new Regex("#(.+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)");
+
+        public string Id { get; private set; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public FabricClaim(string claimString)
+        {
+            var match = claimRegex.Match(claimString);
+
+            Id = match.Groups[1].Value;
+            X = int.Parse(match.Groups[2].Value);
+            Y = int.Parse(match.Groups[3].Value);
+            Width = int.Parse(match.Groups[4].Value);
+            Height = int.Parse(match.Groups[5].Value);
+        }
+
+        public bool HasClaim(int x, int y)
+        {
+            if (x >= X && x < X + Width &&
+                y >= Y && y < Y + Height)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Intersects(FabricClaim other)
+        {
+            return 
+                 ((this.X < other.X + other.Width) && (this.X + this.Width > other.X) &&
+                    (this.Y < other.Y + other.Height) &&(this.Y + this.Height > other.Y));
+        }
+    }
+}
---
+++ AdventCalendar/Day3/FabricMap.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventCalendar.Day3
+{
+    public class FabricMap
+    {
+        private IList<FabricClaim> claims;
+        int[][] claimCounts;
+
+        public FabricMap(IList<FabricClaim> claims, int width, int height)
+        {
+            this.claims = claims;
+            claimCounts = new int[width][];
+
+            for (int x = 0; x < width; x++)
+            {
+                claimCounts[x] = new int[height];
+            }
+        }
+
+        public void GenerateCounts()
+        {
+            for (int x = 0; x < claimCounts.Length; x++)
+            {
+                for (int y = 0; y < claimCounts[x].Length; y++)
+                {
+                    claimCounts[x][y] = claims.Count(c => c.HasClaim(x, y));
+                }
+            }
+        }
+
+        public IList<string> GetOverlaps()
+        {
+            IList<string> overlaps = new List<string>();
+
+            for (int x = 0; x < claimCounts.Length; x++)
+            {
+                for (int y = 0; y < claimCounts[x].Length; y++)
+                {
+                    if (claimCounts[x][y] > 1)
+                    {
+                        overlaps.Add($"{x},{y}");
+                    }
+                }
+            }
+
+            return overlaps;
+        }
+
+        public IList<FabricClaim> GetNonOverlappingClaims()
+        {
+            IList<FabricClaim> claimList = claims.Where(x => claims.Count(y => x.Intersects(y)) == 1).ToList();
+
+            return claimList;
+        }
+    }
+}
