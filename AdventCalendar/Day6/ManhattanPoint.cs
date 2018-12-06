00afcac42c6936137b3253d85132c5692b1b8e04
Tyler Coles <tcoles@hpfc.com>
2018-12-06 10:11:33 -0500

    Finished Day 6

---
+++ AdventCalendar/Day6/ManhattanPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventCalendar.Day6
+{
+    public class ManhattanPoint
+    {
+        public bool IsInfinite { get; set; }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Id { get; set; }
+
+        public int CalculateDistance(int x, int y)
+        {
+            return Math.Abs(X - x) + Math.Abs(Y - y);
+        }
+    }
+}
