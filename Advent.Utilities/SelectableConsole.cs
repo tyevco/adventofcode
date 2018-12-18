a3dc12d63a399f3e3c8f04a3c5dec612a2b1c8ad
Tyler Coles <tcoles@hpfc.com>
2018-12-18 11:35:07 -0500

    Added a selectable console.

---
+++ Advent.Utilities/SelectableConsole.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Advent.Utilities
+{
+    public abstract class SelectableConsole
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetConsoleMode(IntPtr hConsoleHandle, int mode);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool GetConsoleMode(IntPtr handle, out int mode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr GetStdHandle(int handle);
+
+        public SelectableConsole()
+        {
+            var handle = GetStdHandle(-11);
+            int mode;
+            GetConsoleMode(handle, out mode);
+            SetConsoleMode(handle, mode | 0x4);
+        }
+
+        private IList<string> files;
+        public string SelectFileFromFolder(string folder)
+        {
+            int target = 0;
+            files = Directory.GetFiles(folder);
+            bool seekFile = true;
+
+            if (files.Any())
+            {
+                while (seekFile)
+                {
+                    Console.Clear();
+
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        if (i == target)
+                        {
+                            Console.Write("[x] ");
+                        }
+                        else
+                        {
+                            Console.Write("[ ] ");
+                        }
+
+                        Console.WriteLine(files[i]);
+                    }
+
+                    bool keyOkay = false;
+
+                    while (!keyOkay)
+                    {
+                        var info = Console.ReadKey();
+
+                        if (info.Key == ConsoleKey.Enter)
+                        {
+                            seekFile = false;
+                            keyOkay = true;
+                        }
+                        else if (info.Key == ConsoleKey.UpArrow)
+                        {
+                            if (target > 0)
+                                target--;
+                            keyOkay = true;
+                        }
+                        else if (info.Key == ConsoleKey.DownArrow)
+                        {
+                            if (target < files.Count - 1)
+                                target++;
+                            keyOkay = true;
+                        }
+                    }
+                }
+
+                Console.Clear();
+
+                return files[target];
+            }
+            else
+            {
+                Console.WriteLine("No files were found in the specified directory...");
+                return null;
+            }
+        }
+
+        public void Start(string folder)
+        {
+            var file = SelectFileFromFolder(folder);
+
+            if (file != null)
+            {
+                Execute(file);
+            }
+        }
+
+        protected abstract void Execute(string file);
+    }
+}
--- Day15/Program.cs
+++ Day15/Program.cs
@@ -1,55 +1,47 @@
-﻿using System;
+﻿using Advent.Utilities;
+using System;
 using System.Linq;
-using System.Runtime.InteropServices;
 
 namespace Day15
 {
-    class Program
+    class Program : SelectableConsole
     {
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool SetConsoleMode(IntPtr hConsoleHandle, int mode);
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool GetConsoleMode(IntPtr handle, out int mode);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern IntPtr GetStdHandle(int handle);
-
         static void Main(string[] args)
         {
             if (args.Length > 0)
             {
-                var handle = GetStdHandle(-11);
-                int mode;
-                GetConsoleMode(handle, out mode);
-                SetConsoleMode(handle, mode | 0x4);
-
-                var map = new BattleMapParser().ParseData(args[0]);
-
-                var system = new Game(map);
-
-                Console.WriteLine(map);
-
-                while (!system.Finished)
-                {
-                    system.Tick();
-
-                    //Console.Clear();
-                    Console.WriteLine($"Round #{system.Round}");
-                    Console.WriteLine(map);
-                    //System.Threading.Thread.Sleep(100);
-                }
-
-                //Console.Clear();
-                //Console.WriteLine(map);
-                Console.WriteLine($"Combat ends after {system.Round} full rounds");
-                var type = system.Entities.Where(e => e.Health > 0).Select(t => t.Type).FirstOrDefault();
-                var healthRemaining = system.Entities.Where(e => e.Health > 0).Sum(e => e.Health);
-                Console.WriteLine($"{(type == EntityType.Elf ? "Elves" : "Goblins")} win with {healthRemaining} hit points left");
-                Console.WriteLine($"Outcome: {system.Round} * {healthRemaining} =  {healthRemaining * system.Round}");
+                new Program().Start(args[0]);
             }
 
             Console.WriteLine("Finished.");
             Console.ReadLine();
+        }
+
+        protected override void Execute(string file)
+        {
+            var map = new BattleMapParser().ParseData(file);
+
+            var system = new Game(map);
+
+            Console.WriteLine(map);
+
+            while (!system.Finished)
+            {
+                system.Tick();
+
+                //Console.Clear();
+                Console.WriteLine($"Round #{system.Round}");
+                Console.WriteLine(map);
+                //System.Threading.Thread.Sleep(100);
+            }
+
+            //Console.Clear();
+            //Console.WriteLine(map);
+            Console.WriteLine($"Combat ends after {system.Round} full rounds");
+            var type = system.Entities.Where(e => e.Health > 0).Select(t => t.Type).FirstOrDefault();
+            var healthRemaining = system.Entities.Where(e => e.Health > 0).Sum(e => e.Health);
+            Console.WriteLine($"{(type == EntityType.Elf ? "Elves" : "Goblins")} win with {healthRemaining} hit points left");
+            Console.WriteLine($"Outcome: {system.Round} * {healthRemaining} =  {healthRemaining * system.Round}");
         }
     }
 }
