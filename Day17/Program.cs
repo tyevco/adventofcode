21e33e33b75d995803c0aa5aa7c2ebe28a467d6d
Tyler Coles <tcoles@hpfc.com>
2018-12-18 11:36:39 -0500

    Updated Day17 to use console.

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -1,46 +1,51 @@
-﻿using System;
+﻿using Advent.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Day17
 {
-    class Program
+    class Program : SelectableConsole
     {
-
         static void Main(string[] args)
         {
             if (args.Length > 0)
             {
-                IList<Material> active = null;
-
-                var grid = new SpringParser().ParseData(args[0]);
-
-                bool finished = false;
-                int i = 0;
-                while (!finished)
-                {
-                    grid.Propogate();
-                    i++;
-
-                    //Console.WriteLine(grid);
-
-                    if (i % 50 == 0)
-                    {
-                        active = grid.Active();
-                        Console.WriteLine($"[{i.ToString("0000")}] Clay tiles: {active.Count(x => x != null && x.Type == MaterialType.Clay)}, Water tiles: {active.Count(x => x != null && x.Type == MaterialType.Water)}");
-                    }
-
-                    finished = !grid.Added;
-                    //System.Threading.Thread.Sleep(500);
-                }
-
-                active = grid.Active();
-                Console.WriteLine($"[{i.ToString("0000")}] Clay tiles: {active.Count(x => x != null && x.Type == MaterialType.Clay)}, Water tiles: {active.Count(x => x != null && x.Type == MaterialType.Water)}");
-                Console.WriteLine($"Flowing Water tiles: {active.Count(x => x != null && x.Type == MaterialType.Water && ((Water)x).IsFlowing)}");
-
-                Console.ReadLine();
+                new Program().Start(args[0]);
             }
+
+            Console.WriteLine("Finished.");
+            Console.ReadLine();
         }
 
+        protected override void Execute(string file)
+        {
+            IList<Material> active = null;
+
+            var grid = new SpringParser().ParseData(file);
+
+            bool finished = false;
+            int i = 0;
+            while (!finished)
+            {
+                grid.Propogate();
+                i++;
+
+                //Console.WriteLine(grid);
+
+                if (i % 50 == 0)
+                {
+                    active = grid.Active();
+                    Console.WriteLine($"[{i.ToString("0000")}] Clay tiles: {active.Count(x => x != null && x.Type == MaterialType.Clay)}, Water tiles: {active.Count(x => x != null && x.Type == MaterialType.Water)}");
+                }
+
+                finished = !grid.Added;
+                //System.Threading.Thread.Sleep(500);
+            }
+
+            active = grid.Active();
+            Console.WriteLine($"[{i.ToString("0000")}] Clay tiles: {active.Count(x => x != null && x.Type == MaterialType.Clay)}, Water tiles: {active.Count(x => x != null && x.Type == MaterialType.Water)}");
+            Console.WriteLine($"Flowing Water tiles: {active.Count(x => x != null && x.Type == MaterialType.Water && ((Water)x).IsFlowing)}");
+        }
     }
 }
