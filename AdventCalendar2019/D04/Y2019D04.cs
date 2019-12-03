ab20c17c02c86c40438902b4d2ff35b93a60921e
Tyler Coles <tcoles@hpfc.com>
2019-12-03 12:54:28 -0500

    Added 2019D04

---
+++ AdventCalendar2019/D04/Y2019D04.cs
@@ -0,0 +1,18 @@
+﻿using Advent.Utilities;
+using Advent.Utilities.Attributes;
+
+namespace AdventCalendar2019.D04
+{
+    [Exercise("Day 4: **** ")]
+    class Y2019D04 : FileSelectionConsole
+    {
+        public void Execute()
+        {
+            Start("D04/Data");
+        }
+
+        protected override void Execute(string file)
+        {
+        }
+    }
+}
