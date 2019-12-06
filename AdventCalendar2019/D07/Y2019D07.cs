244776c73d83a939d287c7d798cdc7b31d2657ae
Tyler Coles <shidobu@gmail.com>
2019-12-06 01:00:55 -0500

    Adding skeleton for Day 7

---
+++ AdventCalendar2019/D07/Y2019D07.cs
@@ -0,0 +1,21 @@
+﻿using Advent.Utilities;
+using Advent.Utilities.Attributes;
+
+namespace AdventCalendar2019.D07
+{
+    [Exercise("Day 7: ")]
+    class Y2019D07 : FileSelectionConsole, IExercise
+    {
+        public void Execute()
+        {
+            Start("D07/Data");
+        }
+
+        protected override void Execute(string file)
+        {
+            Timer.Monitor(() =>
+            {
+            });
+        }
+    }
+}
