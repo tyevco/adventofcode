4136896e6a512dacd168afb822a8faf4e735513f
Tyler Coles <shidobu@gmail.com>
2019-12-08 09:05:14 -0500

    Finished Day08

---
+++ Advent.Utilities/Helper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Advent.Utilities
+{
+    public static class Helper
+    {
+        public static int ReadIntInput(string message = "Please enter an integer")
+        {
+            int input;
+            Console.Write($"{message}: ");
+            string stdin = Console.ReadLine();
+
+            while (string.IsNullOrWhiteSpace(stdin) || !int.TryParse(stdin, out input))
+            {
+                Console.Write($"{message}: ");
+                stdin = Console.ReadLine();
+            }
+
+            return input;
+        }
+
+        public static T[] CreateArray<T>(int size, T defaultValue)
+        {
+            T[] output = new T[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                output[i] = defaultValue;
+            }
+
+            return output;
+        }
+
+        public static void PrintArray<T>(T[] array, int width = int.MaxValue, string delimiter = "")
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+
+                if (i > 0 && i % width == width - 1)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(delimiter);
+                }
+            }
+        }
+    }
+}
--- AdventCalendar2019/D08/Y2019D08.cs
+++ AdventCalendar2019/D08/Y2019D08.cs
@@ -1,9 +1,12 @@
 ﻿using Advent.Utilities;
 using Advent.Utilities.Attributes;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace AdventCalendar2019.D08
 {
-    [Exercise("Day 8: ")]
+    [Exercise("Day 8: Space Image Format")]
     class Y2019D08 : FileSelectionConsole, IExercise
     {
         public void Execute()
@@ -13,9 +16,82 @@ namespace AdventCalendar2019.D08
 
         protected override void Execute(string file)
         {
+            int width = Helper.ReadIntInput("Width");
+            int height = Helper.ReadIntInput("Height");
+            var fileData = File.ReadAllText(file);
+
+            int[] pixelData = fileData.Select(c => int.Parse(c.ToString())).ToArray();
+
+            Func<int, int, int> getIndex = (int x, int y) => x + y * width;
+
+            int imgSize = width * height;
+
+            int layerCount = pixelData.Length / imgSize;
+
+            char[] finalImage = Helper.CreateArray<char>(imgSize, ' ');
+
             Timer.Monitor(() =>
             {
+                int[][] layers = new int[layerCount][];
+
+                for (int i = 0; i < layerCount; i++)
+                {
+                    int[] layerData = new int[imgSize];
+
+                    for (int x = 0; x < width; x++)
+                    {
+                        for (int y = 0; y < height; y++)
+                        {
+                            var pixelIndex = getIndex(x, y);
+                            layerData[pixelIndex] = pixelData[(imgSize * i) + pixelIndex];
+                        }
+                    }
+
+                    layers[i] = layerData;
+                }
+
+                int fewestZeroCount = int.MaxValue;
+                int[] fewestZeroLayer = null;
+
+                foreach (var layerData in layers)
+                {
+                    int zeroCount = layerData.Count(x => x == 0);
+
+                    if (fewestZeroCount > zeroCount)
+                    {
+                        fewestZeroCount = zeroCount;
+                        fewestZeroLayer = layerData;
+                    }
+                }
+
+                if (fewestZeroLayer != null)
+                {
+                    Helper.PrintArray(fewestZeroLayer, width);
+
+                    int count1 = fewestZeroLayer.Count(x => x == 1);
+                    int count2 = fewestZeroLayer.Count(x => x == 2);
+                    Console.WriteLine($"{count1} x  {count2} = {(count1 * count2)}");
+                }
+                else
+                {
+                    Console.WriteLine("No output found...");
+                }
+
+                for (int i = 0; i < imgSize; i++)
+                {
+                    for (int n = 0; n < layers.Length; n++)
+                    {
+                        if (layers[n][i] != 2)
+                        {
+                            finalImage[i] = layers[n][i] == 1 ? '█' : ' ';
+                            break;
+                        }
+                    }
+                }
+
+                Helper.PrintArray(finalImage, width);
             });
         }
+
     }
 }
