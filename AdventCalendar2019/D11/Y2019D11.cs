4331b97e0964d712011530ceedd2a87d386efc8a
Tyler Coles <tcoles@hpfc.com>
2019-12-10 12:22:25 -0500

    Adding D11 framework.

---
+++ AdventCalendar2019/D11/Y2019D11.cs
@@ -0,0 +1,29 @@
+﻿using Advent.Utilities;
+using Advent.Utilities.Attributes;
+using Advent.Utilities.Intcode;
+using System.IO;
+
+namespace AdventCalendar2019.D09
+{
+    [Exercise("Day 11: ")]
+    class Y2019D11 : FileSelectionConsole, IExercise
+    {
+        public void Execute()
+        {
+            Start("D11/Data");
+        }
+
+        protected override void Execute(string file)
+        {
+            var intcodeData = File.ReadAllText(file);
+
+            Timer.Monitor(() =>
+            {
+                IntcodeProcessor Processor = new IntcodeProcessor(intcodeData);
+                var output = Processor.Process();
+
+                // output.Print();
+            });
+        }
+    }
+}
