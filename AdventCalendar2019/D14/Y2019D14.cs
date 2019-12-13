33e4d2e0ae01ef00b708a2afa8c5ab06d1e48620
Tyler Coles <tcoles@hpfc.com>
2019-12-13 10:10:37 -0500

    Adding Day14

---
+++ AdventCalendar2019/D14/Y2019D14.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Advent.Utilities;
+using Advent.Utilities.Attributes;
+
+namespace AdventCalendar2019.D14
+{
+    [Exercise("Day 14: ")]
+    class Y2019D14 : FileSelectionConsole, IExercise
+    {
+        public void Execute()
+        {
+            Start("D14/Data");
+        }
+
+        protected override void Execute(string file)
+        {
+            var lines = File.ReadAllLines(file);
+
+            Timer.Monitor(() =>
+            {
+
+            });
+        }
+    }
+}
