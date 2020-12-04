9e843b8b397be1fe90b1d9589678739112b065f5
Tyler Coles <shidobu@gmail.com>
2020-12-04 00:48:32 -0500

    more
    fixing typo

--- AdventCalendar2020/D03/Y2020D03.cs
+++ AdventCalendar2020/D03/Y2020D03.cs
@@ -1,28 +1,71 @@
-using Advent.Utilities;
+﻿using Advent.Utilities;
 using Advent.Utilities.Attributes;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventCalendar2020.D03
 {
     [Exercise("Day 3: Toboggan Trajectory")]
-    class Y2020D03 : FileSelectionParsingConsole<IList<string>>, IExercise
+    class Y2020D03 : FileSelectionParsingConsole<int[][]>, IExercise
     {
         public void Execute()
         {
             Start("D03/Data");
         }
 
-        protected override IList<string> DeserializeData(IList<string> data)
+        protected override int[][] DeserializeData(IList<string> data)
         {
-            // Setup data parser.
-            return data;
+            return data.Select(x => x.Select(i => i == '.' ? 0 : 1).ToArray()).ToArray();
         }
 
-        protected override void Execute(IList<string> data)
+        protected override void Execute(int[][] data)
         {
-            // perform task.
+            {
+                int x = 0, y = 0;
+                int width = data[0].Length;
+
+                int trees = 0;
+                while (y < data.Length)
+                {
+                    x = (x + 3) % width;
+                    y = y + 1;
+                    if (y >= data.Length)
+                        break;
+
+                    trees += data[y][x];
+                }
+
+                AnswerPartOne(trees);
+            }
+
+            {
+                var xSlopes = new int[] { 1, 3, 5, 7, 1 };
+                var ySlopes = new int[] { 1, 1, 1, 1, 2 };
+                List<long> mult = new List<long>(); ;
+
+                for (int i = 0; i < xSlopes.Length; i++)
+                {
+                    int x = 0, y = 0;
+                    int width = data[0].Length;
+
+                    int trees = 0;
+                    while (y < data.Length)
+                    {
+                        x = (x + xSlopes[i]) % width;
+                        y = y + ySlopes[i];
+                        if (y >= data.Length)
+                            break;
+
+                        trees += data[y][x];
+                    }
+
+                    System.Console.WriteLine($"{trees}");
+
+                    mult.Add(trees);
+                }
+
+                AnswerPartTwo(mult.Aggregate((a, b) => a * b));
+            }
         }
     }
 }
