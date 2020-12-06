2542562759350581035be80fc9e23e9a132e02c5
Tyler Coles <shidobu@gmail.com>
2020-12-06 00:11:45 -0500

    Finished Day6

--- AdventCalendar2020/D06/Y2020D06.cs
+++ AdventCalendar2020/D06/Y2020D06.cs
@@ -1,27 +1,70 @@
 ﻿using Advent.Utilities;
 using Advent.Utilities.Attributes;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventCalendar2020.D06
 {
-    [Exercise("Day 6: ")]
-    class Y2020D06 : FileSelectionParsingConsole<IList<string>>, IExercise
+    [Exercise("Day 6: Custom Customs")]
+    class Y2020D06 : FileSelectionParsingConsole<IList<CustomsGroup>>, IExercise
     {
         public void Execute()
         {
             Start("D06/Data");
         }
 
-        protected override IList<string> DeserializeData(IList<string> data)
+        protected override IList<CustomsGroup> DeserializeData(IList<string> data)
         {
-            return data;
+            IList<CustomsGroup> customsList = new List<CustomsGroup>();
+
+            CustomsGroup customs = new CustomsGroup();
+            foreach (var line in data)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    customsList.Add(customs);
+                    customs = new CustomsGroup();
+                }
+                else
+                {
+                    customs.People.Add(new PersonAnswers
+                    {
+                        Answers = string.Join("", line.ToCharArray().Distinct()),
+                    });
+                }
+            }
+
+            customsList.Add(customs);
+
+            return customsList;
         }
 
-        protected override void Execute(IList<string> data)
+        protected override void Execute(IList<CustomsGroup> data)
         {
+            int totalAnyCount = 0;
+            int totalAllCount = 0;
+            var alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            foreach (var group in data)
+            {
+                int groupAnyCount = 0;
+                foreach(var c in alphabet)
+                {
+                    if (group.People.Any(p => p.Answers.Contains(c)))
+                    {
+                        groupAnyCount++;
+                    }
 
-            AnswerPartOne(string.Empty);
-            AnswerPartTwo(string.Empty);
+                    if (group.People.All(p => p.Answers.Contains(c)))
+                    {
+                        totalAllCount++;
+                    }
+                }
+
+                totalAnyCount += groupAnyCount;
+            }
+
+            AnswerPartOne(totalAnyCount);
+            AnswerPartTwo(totalAllCount);
         }
     }
 }
