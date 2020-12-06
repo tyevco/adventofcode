d3a433723ed4ff3a2154f39106d6ffefc5c82603
Tyler Coles <shidobu@gmail.com>
2020-12-06 00:15:40 -0500

    Prep for Day7

---
+++ AdventCalendar2020/D07/Y2020D07.cs
@@ -0,0 +1,27 @@
+﻿using Advent.Utilities;
+using Advent.Utilities.Attributes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventCalendar2020.D07
+{
+    [Exercise("Day 7: ")]
+    class Y2020D07 : FileSelectionParsingConsole<IList<string>>, IExercise
+    {
+        public void Execute()
+        {
+            Start("D07/Data");
+        }
+
+        protected override IList<string> DeserializeData(IList<string> data)
+        {
+            return data;
+        }
+
+        protected override void Execute(IList<string> data)
+        {
+            AnswerPartOne(string.Empty);
+            AnswerPartTwo(string.Empty);
+        }
+    }
+}
