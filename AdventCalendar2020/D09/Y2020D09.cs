5439426cf67fbbde788a39355b3be2b2eef29d89
Tyler Coles <shidobu@gmail.com>
2020-12-09 00:38:25 -0500

    Converting to longs before execute

--- AdventCalendar2020/D09/Y2020D09.cs
+++ AdventCalendar2020/D09/Y2020D09.cs
@@ -6,27 +6,26 @@ using System.Linq;
 namespace AdventCalendar2020.D09
 {
     [Exercise("Day 9: Encoding Error")]
-    class Y2020D09 : FileSelectionParsingConsole<IList<string>>, IExercise
+    class Y2020D09 : FileSelectionParsingConsole<IList<long>>, IExercise
     {
         public void Execute()
         {
             Start("D09/Data");
         }
 
-        protected override IList<string> DeserializeData(IList<string> data)
+        protected override IList<long> DeserializeData(IList<string> data)
         {
-            return data;
+            return data.Select(x => long.Parse(x)).ToList();
         }
 
-        protected override void Execute(IList<string> data)
+        protected override void Execute(IList<long> data)
         {
             Queue<long> preamble = new Queue<long>();
             int count = 25;
             long partOne = 0;
 
-            foreach (var item in data)
+            foreach (var curr in data)
             {
-                var curr = long.Parse(item);
                 if (preamble.Count < count)
                 {
                     preamble.Enqueue(curr);
@@ -49,10 +48,8 @@ namespace AdventCalendar2020.D09
             }
 
             Queue<long> sums = new Queue<long>();
-            foreach (var item in data)
+            foreach (var curr in data)
             {
-                var curr = long.Parse(item);
-
                 sums.Enqueue(curr);
 
                 while (sums.Sum() > partOne)
