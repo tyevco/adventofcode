3e7a70bdf11fb1bee338fa42f7c58f2249031480
Tyler Coles <shidobu@gmail.com>
2020-12-09 15:46:13 -0500

    setup day 10

---
+++ AdventCalendar2020/D10/Y2020D10.cs
@@ -0,0 +1,31 @@
+﻿using Advent.Utilities;
+using Advent.Utilities.Attributes;
+using System.Collections.Generic;
+
+namespace AdventCalendar2020.D10
+{
+    [Exercise("Day 10: ")]
+    class Y2020D10 : FileSelectionParsingConsole<IList<string>>, IExercise
+    {
+        public void Execute()
+        {
+            Start("D10/Data");
+        }
+
+        protected override IList<string> DeserializeData(IList<string> data)
+        {
+
+
+            return data;
+        }
+
+        protected override void Execute(IList<string> data)
+        {
+
+
+
+            AnswerPartOne($"");
+            AnswerPartTwo($"");
+        }
+    }
+}
