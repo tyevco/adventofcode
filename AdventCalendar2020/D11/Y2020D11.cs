08360a4fd70e853b39107f80b5eb64d3c73c07aa
Tyler Coles <shidobu@gmail.com>
2020-12-10 16:04:25 -0500

    Set up for Day11

---
+++ AdventCalendar2020/D11/Y2020D11.cs
@@ -0,0 +1,26 @@
+﻿using Advent.Utilities;
+using Advent.Utilities.Attributes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventCalendar2020.D11
+{
+    [Exercise("Day 11: ")]
+    class Y2020D11 : FileSelectionParsingConsole<IList<string>>, IExercise
+    {
+        public void Execute()
+        {
+            Start("D11/Data");
+        }
+
+        protected override IList<string> DeserializeData(IList<string> data)
+        {
+            return data;
+        }
+
+        protected override void Execute(IList<string> data)
+        {
+            
+        }
+    }
+}
