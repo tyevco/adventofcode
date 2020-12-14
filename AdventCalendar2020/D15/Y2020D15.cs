7f7b935ca743c3508fce2894b645aadbce2ebe4e
Tyler Coles <shidobu@gmail.com>
2020-12-14 00:57:48 -0500

    Day 15 prep

---
+++ AdventCalendar2020/D15/Y2020D15.cs
@@ -0,0 +1,40 @@
+﻿using Advent.Utilities;
+using Advent.Utilities.Attributes;
+using System.Collections.Generic;
+
+namespace AdventCalendar2020.D15
+{
+    [Exercise("Day 15: ")]
+    class Y2020D15 : FileSelectionParsingConsole<IList<string>>, IExercise
+    {
+        public void Execute()
+        {
+            Start("D15/Data");
+        }
+
+        protected override IList<string> DeserializeData(IList<string> data)
+        {
+            return data;
+        }
+
+        protected override void Execute(IList<string> data)
+        {
+            PartOne(data);
+            PartTwo(data);
+        }
+
+
+        protected void PartOne(IList<string> data)
+        {
+
+            AnswerPartOne(data);
+        }
+
+
+        protected void PartTwo(IList<string> data)
+        {
+
+            AnswerPartTwo(data);
+        }
+    }
+}
